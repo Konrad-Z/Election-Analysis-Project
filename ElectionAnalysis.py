0a3519ded87d039f7b141ba5f8a9b3eb6acf09e3
N1179676 <konrad.zajac2024@my.ntu.ac.uk>
2024-11-14 21:12:23 +0000

    Main Menu
    Implemented a basic main menu which displays options

---
+++ ElectionAnalysis.py
@@ -0,0 +1,12 @@
+MainOptions = ['List the MPs', 'List the constituencies', 'List the Parties']
+
+# Main Menu
+
+print('\nWelcome to the 2024 General Election Analysis App')
+print('Choose your option - Use the number')
+print('#\t Option')
+
+optionNumber = 0
+for option in MainOptions:
+    print(optionNumber, '\t', option)
+    optionNumber += 1
